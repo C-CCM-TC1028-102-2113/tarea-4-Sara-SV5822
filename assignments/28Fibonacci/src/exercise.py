1d748002f1ca91d0b2b2a56b696d61ade2303bb3
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-09-03 14:58:26 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,24 @@
-
 def main():
+    index = int(input("Enter the index: "))
     #escribe tu código abajo de esta línea
+
+    num1=0
+    num2=1
+    result=0
+    if index!=0 and index!=1:
+
+        for i in range (1,index):
+            result= num1 + num2
+            num1= num2
+            num2= result
+        print (str(result))
+
+    elif index==0:
+        print('0')
+    elif index==1:
+        print('1')
+
     pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
