d866b4a4491528b619a4b06dc56f5f8bfdc6f906
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-09-03 13:36:32 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,19 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+  #escribe tu código abajo de esta línea
+    n=int(input(''))
+    num=0
 
-if __name__=='__main__':   
+    while num<n:
+        num=num+1
+
+        if num%2!=0:
+            sig='#'
+        elif num%2==0:
+            sig='%'
+
+        print(str(num)+'-'+str(sig))
+
+
+
+if __name__ == '__main__':
     main()
