2fe28ab7af63887040cc3d0e1ecf7c15e6a6bd49
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-09-03 19:58:54 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,23 +1,15 @@
-import sys
 def main():
     height = int(input("Enter triangle height: "))
     #escribe tu código abajo de esta línea
-    if height!=0:
-        for i in range (0,height):
-            n=0
-            k=height
-            while k>i+1:
-                sys.stdout.write(' ')
-                k=k-1
-            else: 
 
-                while n<i+1:
-                    sys.stdout.write('*')
-                    n=n+1
-            print('')
-        pass
-    elif height==0:
-        pass
+    for i in range (height+1):
+        space=height-i
+        a=' '*space+'*'*i
+        print(a)
+
+
+
+    pass
 
 if __name__ == '__main__':
     main()
